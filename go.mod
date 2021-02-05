e073c343395ec43097bc52ea7d644095dda12ded
James Strachan <james.strachan@gmail.com>
2021-02-05 16:42:57 +0000

    fix: add a verify pods command
    so that we can remove any pod thats failing to load the image
    
    as we sometimes see this issue on GCP if an image can't be pulled yet; removing the pod helps get k8s unstuck in BDD tests

--- go.mod
+++ go.mod
@@ -7,12 +7,12 @@ require (
 	github.com/cpuguy83/go-md2man v1.0.10
 	github.com/genkiroid/cert v0.0.0-20191007122723-897560fbbe50
 	github.com/jenkins-x/jx-api/v4 v4.0.24
-	github.com/jenkins-x/jx-helpers/v3 v3.0.75
+	github.com/jenkins-x/jx-helpers/v3 v3.0.77
 	github.com/jenkins-x/jx-logging/v3 v3.0.3
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/pflag v1.0.5
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	github.com/stretchr/testify v1.6.1
 	k8s.io/api v0.20.2
 	k8s.io/apimachinery v0.20.2
 	k8s.io/client-go v11.0.0+incompatible
---
+++ pkg/cmd/pods/pods.go
@@ -0,0 +1,194 @@
+package pods
+
+import (
+	"context"
+	"fmt"
+	"github.com/jenkins-x/jx-helpers/v3/pkg/kube/pods"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/tools/cache"
+	"os"
+	"sync/atomic"
+	"time"
+
+	"github.com/jenkins-x/jx-helpers/v3/pkg/cobras/helper"
+	"github.com/jenkins-x/jx-helpers/v3/pkg/cobras/templates"
+	"github.com/jenkins-x/jx-helpers/v3/pkg/kube"
+	"github.com/jenkins-x/jx-logging/v3/pkg/log"
+	"github.com/jenkins-x/jx-verify/pkg/rootcmd"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"k8s.io/client-go/kubernetes"
+)
+
+// ErrImagePullMessage the message on an event if it can't pull a pod
+const ErrImagePullMessage = "Error: ErrImagePull"
+
+//  ErrImagePullBackOffMessage if we are backing off
+const ErrImagePullBackOffMessage = "Error: ImagePullBackOff"
+
+var (
+	cmdLong = templates.LongDesc(`
+		Verifies that all pods start OK in the current namespace; killing any Pods which have ErrImagePul
+`)
+
+	cmdExample = templates.Examples(`
+		# populate the pods don't have missing images
+		jx verify pods
+
+			`)
+)
+
+type Options struct {
+	KubeClient kubernetes.Interface
+	Namespace  string
+	Selector   string
+	PodCount   int
+	IsReady    atomic.Value
+	readyPods  map[string]bool
+	stop       chan struct{}
+}
+
+func NewCmdVerifyPods() (*cobra.Command, *Options) {
+	o := &Options{}
+
+	cmd := &cobra.Command{
+		Use:     "pods",
+		Short:   "Verifies that all pods start OK in the current namespace; killing any Pods which have ErrImagePull",
+		Long:    cmdLong,
+		Example: fmt.Sprintf(cmdExample, rootcmd.BinaryName),
+		Run: func(cmd *cobra.Command, args []string) {
+			err := o.Run()
+			helper.CheckErr(err)
+		},
+	}
+	cmd.Flags().StringVarP(&o.Namespace, "namespace", "n", "", "The namespace to look for events")
+	cmd.Flags().StringVarP(&o.Selector, "selector", "s", "", "The selector to query for all pods being running")
+	cmd.Flags().IntVarP(&o.PodCount, "count", "c", 1, "The minimum Ready pod count required matching the selector before terminating")
+
+	return cmd, o
+}
+
+func (o *Options) Run() error {
+	var err error
+
+	if o.readyPods == nil {
+		o.readyPods = map[string]bool{}
+	}
+	o.KubeClient, o.Namespace, err = kube.LazyCreateKubeClientAndNamespace(o.KubeClient, o.Namespace)
+	if err != nil {
+		return errors.Wrapf(err, "failed to create kubernetes client")
+	}
+
+	o.stop = make(chan struct{})
+	defer close(o.stop)
+	defer runtime.HandleCrash()
+
+	informerFactory := informers.NewSharedInformerFactoryWithOptions(
+		o.KubeClient,
+		time.Minute*10,
+		informers.WithNamespace(o.Namespace),
+	)
+
+	eventInformer := informerFactory.Core().V1().Events().Informer()
+
+	eventInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			e := obj.(*v1.Event)
+			if e == nil {
+				log.Logger().Warnf("no Event found for %v", obj)
+				return
+			}
+			o.OnEvent(e, o.Namespace)
+			log.Logger().Debugf("added Event %s", e.Name)
+		},
+		UpdateFunc: func(old, obj interface{}) {
+			e := obj.(*v1.Event)
+			if e == nil {
+				log.Logger().Warnf("no Event found for %v", obj)
+				return
+			}
+			o.OnEvent(e, o.Namespace)
+			log.Logger().Debugf("updated Event %s", e.Name)
+		},
+	})
+
+	podInformer := informerFactory.Core().V1().Pods().Informer()
+
+	podInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			p := obj.(*v1.Pod)
+			o.OnPod(p, o.Namespace)
+			log.Logger().Debugf("added Pod %s", p.Name)
+		},
+		UpdateFunc: func(old, new interface{}) {
+			p := new.(*v1.Pod)
+			o.OnPod(p, o.Namespace)
+			log.Logger().Debugf("updated Pod %s", p.Name)
+		},
+	})
+
+	// Starts all the shared informers that have been created by the factory so
+	// far.
+
+	informerFactory.Start(o.stop)
+
+	// wait for the initial synchronization of the local cache
+	if !cache.WaitForCacheSync(o.stop, eventInformer.HasSynced) {
+		runtime.HandleError(fmt.Errorf("timed out waiting for event caches to sync"))
+	}
+	if !cache.WaitForCacheSync(o.stop, podInformer.HasSynced) {
+		runtime.HandleError(fmt.Errorf("timed out waiting for pod caches to sync"))
+	}
+	o.IsReady.Store(true)
+
+	<-o.stop
+
+	// Wait forever
+	select {}
+}
+
+func (o *Options) OnEvent(e *v1.Event, namespace string) {
+	if e.InvolvedObject.Kind != "Pod" {
+		return
+	}
+	if e.Message != ErrImagePullMessage && e.Message != ErrImagePullBackOffMessage {
+		log.Logger().Debugf("ignoring pod message %s", e.Message)
+		return
+	}
+
+	ns := e.InvolvedObject.Namespace
+	if ns == "" {
+		ns = namespace
+	}
+	name := e.InvolvedObject.Name
+	log.Logger().Infof("found pod %s with message %s", name, e.Message)
+
+	ctx := context.TODO()
+	err := o.KubeClient.CoreV1().Pods(ns).Delete(ctx, name, metav1.DeleteOptions{})
+	if err != nil {
+		log.Logger().Errorf("failed to delete Pod %s in namespace %s : %s", name, ns, err.Error())
+		return
+	}
+
+	log.Logger().Infof("deleted pod %s in namespace %s", name, ns)
+}
+
+func (o *Options) OnPod(p *v1.Pod, namespace string) {
+	name := p.Name
+	if pods.IsPodReady(p) {
+		o.readyPods[name] = true
+	} else {
+		delete(o.readyPods, name)
+	}
+
+	count := len(o.readyPods)
+	if count < o.PodCount {
+		return
+	}
+
+	log.Logger().Infof("has %d ready pods now", count)
+	os.Exit(0)
+}
